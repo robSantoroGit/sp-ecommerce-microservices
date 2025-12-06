7b19f12fbe865aab01b23f29962c3c5d0499b43d
rob <rob@rob.com>
2025-12-06 13:59:22 +0100

    user-service modified pom.xml and moved StartupApplication

--- user-service/src/main/java/com/ecommerce/userService/bin/UserServiceApplication.java
+++ user-service/src/main/java/com/ecommerce/userService/bin/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.userService;
+package com.ecommerce.userService.bin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
